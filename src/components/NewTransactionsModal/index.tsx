b46d36e22a2a128216be78b4e77494dcd00fe495
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2022-10-04 10:13:21 -0300

    Customizando Botoes

--- src/components/NewTransactionsModal/index.tsx
+++ src/components/NewTransactionsModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from "react-modal";
-import closeImg from "../../assets/close.svg"
-import { Container } from "./styles";
+import closeImg from "../../assets/close.svg";
+import outcomeImg from "../../assets/outcome.svg";
+import incomeImg from "../../assets/income.svg";
+import { Container, TransactionTypeContainer } from "./styles";
 
 interface NewTransactionsModalProps {
     isOpen: boolean;
@@ -25,6 +27,18 @@ export function NewTransactionsModal({ isOpen, onRequestClose }: NewTransactions
          <input placeholder="Título"/>
 
          <input type="number" placeholder="Valor"/>
+
+         <TransactionTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+
+          <button type="button">
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+         </TransactionTypeContainer>
          
          <input placeholder="Categoria"/>
         
