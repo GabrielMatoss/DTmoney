44dcc31ba5d93c166d5fdf0fc24a4eaa3e7be0d7
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2022-10-06 08:56:28 -0300

    arrumando  bugs

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,11 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "./styles";
 
+interface Transaction{
+    id: number;
+    title: string;
+    type: string;
+    category: string;
+    amout: number;
+    createdAt: string;
+}
+
 export function TransactionsTable() {
+    const[transactions, setTransactions] = useState<Transaction[]>([]);
+
     useEffect(() => {
         api.get("transactions")
-            .then(response => console.log(response.data))
+            .then(response => setTransactions(response.data.transactions))
     }, []);
 
 
@@ -22,18 +33,14 @@ export function TransactionsTable() {
                 </thead>
 
                 <tbody>
-                    <tr>
-                        <td>Desenvolvimento de website</td>
-                        <td className="deposit">R$12.000</td>
-                        <td>Desenvolvimento</td>
-                        <td>20/02/2022</td>
+                    {transactions.map(transaction => (
+                    <tr key={transaction.id}>
+                        <td>{transaction.title}</td>
+                        <td className={transaction.type}>{transaction.amout}</td>
+                        <td>{transaction.category}</td>
+                        <td>{transaction.createdAt}</td>
                     </tr>
-                    <tr>
-                        <td>Aluguel</td>
-                        <td className="withdraw">- R$1.500</td>
-                        <td>Casa</td>
-                        <td>25/02/2022</td>
-                    </tr>
+                        ))}
                 </tbody>
             </table>
         </Container>
