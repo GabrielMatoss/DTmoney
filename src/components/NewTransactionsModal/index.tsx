5f859979e7511c302f1720c52ca2170f61da60dc
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2022-10-07 09:54:06 -0300

    movendo criação para context

--- src/components/NewTransactionsModal/index.tsx
+++ src/components/NewTransactionsModal/index.tsx
@@ -1,12 +1,12 @@
-import { FormEvent, useState } from "react";
-import { api } from "../../services/api";
+import { FormEvent, useState, useContext } from "react";
 import Modal from "react-modal";
-import { Container, TransactionTypeContainer, RadioBox } from "./styles";
+import { TransactionsContext } from "../../TransactionsContext";
 
 import closeImg from "../../assets/close.svg";
 import outcomeImg from "../../assets/outcome.svg";
 import incomeImg from "../../assets/income.svg";
 
+import { Container, TransactionTypeContainer, RadioBox } from "./styles";
 
 
 interface NewTransactionsModalProps {
@@ -15,22 +15,22 @@ interface NewTransactionsModalProps {
 }
 
 export function NewTransactionsModal({ isOpen, onRequestClose }: NewTransactionsModalProps) {
+  const { createTransaction } = useContext(TransactionsContext);
+
   const [title, setTitle] = useState("");
-  const [value, setValue] = useState(0);
+  const [amount, setAmount] = useState(0);
   const [category, setCategory] = useState("");
   const [type, setType] = useState("deposit");
 
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    const data = { 
+    createTransaction({
       title,
-      value,
+      amount,
       category,
-      type,
-      };
-
-    api.post("/transactions", data)
+      type
+    })
   }
 
   return (
@@ -60,8 +60,8 @@ export function NewTransactionsModal({ isOpen, onRequestClose }: NewTransactions
         <input
           type="number"
           placeholder="Valor"
-          value={value}
-          onChange={event => setValue(Number(event.target.value))}
+          value={amount}
+          onChange={event => setAmount(Number(event.target.value))}
         />
 
         <TransactionTypeContainer>
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -7,9 +7,9 @@ import totalImg from "../../assets/total.svg";
 import { Container } from "./styles";
 
 export function Summary() {
-    const transactions = useContext(TransactionsContext);
+    const { transactions } = useContext(TransactionsContext);
 
-    console.log(transactions)
+    console.log(transactions);
 
     return (
         <Container>
